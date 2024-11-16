d8fa48488e5fab99cd4a942a1d32000e5011a5a8
Regis JOUAN <regis.jouan@lapiscine.pro>
2024-11-16 12:30:01 +0100

    ajout de la clé homepage dans package.json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import TentangkamiPage from './pages/TentangkamiPage';
+import ProduksPage from './pages/ProduksPage';
+import KontakPage from './pages/KontakPage';
+import FranchisePage from './pages/FranchisePage';
+import LegalPage from './pages/LegalPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/tentangkami" element={<TentangkamiPage />} /> 
+          <Route path="/produk" element={<ProduksPage />} /> 
+          <Route path="/kontak" element={<KontakPage />} />
+          <Route path="/franchise" element={< FranchisePage/>} />
+          <Route path="/legal" element={< LegalPage/>} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
